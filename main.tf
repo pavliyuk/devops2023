38ed0b77f7e55f1326f8ce0de141156ae1a8f132
pavliyuk <54590039+pavliyuk@users.noreply.github.com>
2023-03-03 15:08:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
     region = "us-east-1"  
-   environment {
-        AWS_ACCESS_KEY_ID     = credentials('aws_access_key_id')
-        AWS_SECRET_ACCESS_KEY = credentials('aws_secret_access_key')
-}
+environment {
+        AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
+        AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+    }
 
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
