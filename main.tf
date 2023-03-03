d925e7cb912b57d2d1258daf9614e240bcd59a1f
pavliyuk <54590039+pavliyuk@users.noreply.github.com>
2023-03-03 15:45:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
-    resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
 }
