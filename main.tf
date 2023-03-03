ee8e41cd8389f7b6173c1b0bd0bd34f6d7a478c5
pavliyuk <54590039+pavliyuk@users.noreply.github.com>
2023-03-03 16:37:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+ environment {
+        AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+    }
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
