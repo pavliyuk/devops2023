6e91b32e9a0e45b1a5083ca81391e65b51541457
pavliyuk <54590039+pavliyuk@users.noreply.github.com>
2023-03-08 15:30:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,49 @@
+# Встановлюємо посилання на постачальника Amazon Web Services (AWS)
+#############################################
+#####                                   #####
+#####---------FINAL-PROJECT-------------#####
+#####                                   #####
+#############################################
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+##Create es2 instance
+resource "aws_instance" "wordpress" {
+  ami                    = "ami-006dcf34c09e50022"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tcp.id]
+  user_data              = file("init.sh")
+  tags = {
+    Name = "Wordpress"
+    Owner = "Alex"
+  }
 }
+###Create security_group
+resource "aws_security_group" "allow_tcp" {
+  name        = "allow_tcp"
+  description = "Allow TCP inbound traffic"
 
+  #-------------------------HTTPS-Server-Enable-------------------------------
+  dynamic "ingress" {
+    for_each = [ "22", "80", "443" ]
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      description = "tcp_port"
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tcp"
+  }
+}
